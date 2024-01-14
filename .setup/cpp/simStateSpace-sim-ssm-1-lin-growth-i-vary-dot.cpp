80873cf1021aeecbccffc79e21a94fb4d4ddb78d
jeksterslab <jeksterslab@users.noreply.github.com>
2024-01-14 23:10:18 +0000

    Automated build.
    Files changed:
    A	.Rbuildignore
    A	.github/linters/.lintr
    A	.setup/build/simStateSpace.pdf
    A	.setup/build/simStateSpace_1.1.0.tar.gz
    M	.setup/cpp/simStateSpace-ou-2-ssm.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-0-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-0-fixed-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-0-i-vary-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-0-lin-growth-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-0-lin-growth-i-vary-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-0-ou-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-0-ou-fixed-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-0-ou-i-vary-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-0-var-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-0-var-fixed-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-0-var-i-vary-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-1-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-1-fixed-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-1-i-vary-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-1-lin-growth-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-1-lin-growth-i-vary-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-1-ou-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-1-ou-fixed-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-1-ou-i-vary-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-1-var-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-1-var-fixed-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-1-var-i-vary-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-2-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-2-fixed-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-2-i-vary-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-2-lin-growth-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-2-lin-growth-i-vary-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-2-ou-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-2-ou-fixed-dot.cpp
    M	.setup/cpp/simStateSpace-sim-ssm-2-ou-i-vary-dot.cpp
    A	.setup/latex/bib/quarto.bib
    A	.setup/lint/.lintr
    A	.setup/quarto/bib/quarto.bib
    A	CITATION.cff
    M	DESCRIPTION
    A	LICENSE.md
    A	NAMESPACE
    A	R/RcppExports.R
    A	README.md
    A	man/OU2SSM.Rd
    A	man/SimSSM.Rd
    A	man/SimSSMFixed.Rd
    A	man/SimSSMIVary.Rd
    A	man/SimSSMLinGrowth.Rd
    A	man/SimSSMLinGrowthIVary.Rd
    A	man/SimSSMOU.Rd
    A	man/SimSSMOUFixed.Rd
    A	man/SimSSMOUIVary.Rd
    A	man/SimSSMVAR.Rd
    A	man/SimSSMVARFixed.Rd
    A	man/SimSSMVARIVary.Rd
    A	man/as.data.frame.simstatespace.Rd
    A	man/as.matrix.simstatespace.Rd
    A	man/plot.simstatespace.Rd
    A	man/print.simstatespace.Rd
    A	man/simStateSpace-package.Rd
    M	project.Rproj
    A	src/Makevars
    A	src/Makevars.win
    A	src/RcppExports.cpp
    A	src/source.cpp
    A	vignettes/fig-vignettes-lin-growth-unnamed-chunk-11-1.png
    A	vignettes/fig-vignettes-lin-growth-unnamed-chunk-4-1.png
    A	vignettes/fig-vignettes-ou-unnamed-chunk-12-1.png
    A	vignettes/fig-vignettes-ou-unnamed-chunk-4-1.png
    A	vignettes/fig-vignettes-ou-unnamed-chunk-4-2.png
    A	vignettes/fig-vignettes-ou-unnamed-chunk-4-3.png
    A	vignettes/fig-vignettes-var-unnamed-chunk-11-1.png
    A	vignettes/fig-vignettes-var-unnamed-chunk-4-1.png
    A	vignettes/fig-vignettes-var-unnamed-chunk-4-2.png
    A	vignettes/fig-vignettes-var-unnamed-chunk-4-3.png
    A	vignettes/lin-growth.Rmd
    A	vignettes/lin-growth.c
    A	vignettes/lin-growth.o
    A	vignettes/lin-growth.so
    A	vignettes/ou.Rmd
    A	vignettes/ou.c
    A	vignettes/ou.o
    A	vignettes/ou.so
    A	vignettes/var.Rmd
    A	vignettes/var.c
    A	vignettes/var.o
    A	vignettes/var.so
    A	vignettes/vignettes.bib

--- .setup/cpp/simStateSpace-sim-ssm-1-lin-growth-i-vary-dot.cpp
+++ .setup/cpp/simStateSpace-sim-ssm-1-lin-growth-i-vary-dot.cpp
@@ -6,7 +6,11 @@
 #include <RcppArmadillo.h>
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export(.SimSSM1LinGrowthIVary)]]
-Rcpp::List SimSSM1LinGrowthIVary(const int n, const Rcpp::List& mu0, const Rcpp::List& sigma0_l, const Rcpp::List& theta_l, const Rcpp::List& gamma_eta, const Rcpp::List& x, const int time) {
+Rcpp::List SimSSM1LinGrowthIVary(const int n, const Rcpp::List& mu0,
+                                 const Rcpp::List& sigma0_l,
+                                 const Rcpp::List& theta_l,
+                                 const Rcpp::List& gamma_eta,
+                                 const Rcpp::List& x, const int time) {
   // Step 1: Create constant vectors and matrices
   arma::mat lambda = {{1, 0}};
   arma::mat beta = {{1, 1}, {0, 1}};
@@ -27,7 +31,8 @@ Rcpp::List SimSSM1LinGrowthIVary(const int n, const Rcpp::List& mu0, const Rcpp:
     arma::mat gamma_eta_temp = gamma_eta[i];
 
     // Step 3.2: Generate initial condition
-    eta.col(0) = mu0_temp + (sigma0_l_temp * arma::randn(2)) + (gamma_eta_temp * x_t.col(0));
+    eta.col(0) = mu0_temp + (sigma0_l_temp * arma::randn(2)) +
+                 (gamma_eta_temp * x_t.col(0));
     y.col(0) = (lambda * eta.col(0)) + (theta_l_temp * arma::randn(1));
 
     // Step 3.3: Simulate state space model data using a loop
@@ -41,7 +46,11 @@ Rcpp::List SimSSM1LinGrowthIVary(const int n, const Rcpp::List& mu0, const Rcpp:
     id.fill(i + 1);
 
     // Step 3.5: Save the transposed data matrices in a list
-    out[i] = Rcpp::List::create(Rcpp::Named("id") = id, Rcpp::Named("time") = arma::regspace(0, time - 1), Rcpp::Named("y") = y.t(), Rcpp::Named("eta") = eta.t(), Rcpp::Named("x") = x_t.t());
+    out[i] = Rcpp::List::create(
+        Rcpp::Named("id") = id,
+        Rcpp::Named("time") = arma::regspace(0, time - 1),
+        Rcpp::Named("y") = y.t(), Rcpp::Named("eta") = eta.t(),
+        Rcpp::Named("x") = x_t.t());
   }
 
   // Step 4: Return the results
