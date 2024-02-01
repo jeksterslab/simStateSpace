9cacb593e72cf4085f6972e62395fef98e0c61e6
jeksterslab <learn.jeksterslab@gmail.com>
2024-02-01 20:31:22 +0000

    Automated build.

--- .setup/cpp/simStateSpace-sim-ssm-lat-fixed-1-dot.cpp
+++ .setup/cpp/simStateSpace-sim-ssm-lat-fixed-1-dot.cpp
@@ -8,22 +8,19 @@
 // [[Rcpp::export(.SimSSMLatFixed1)]]
 Rcpp::List SimSSMLatFixed1(const int n, const int time, const double delta_t,
                            const arma::vec& mu0, const arma::mat& sigma0_l,
-                           const arma::vec& alpha, const arma::mat& beta,
-                           const arma::mat& psi_l, const Rcpp::List& x,
-                           const arma::mat& gamma_eta) {
+                           const arma::vec& alpha, const arma::mat& beta, const arma::mat& psi_l,
+                           const Rcpp::List& x, const arma::mat& gamma_eta) {
   // Step 1: Determine dimensions
-  int p = mu0.n_elem;  // number of latent variables
-  int k = p;           // number of observed variables
-  arma::vec time_vec =
-      arma::linspace(0, (time - 1) * delta_t, time);  // time vector
+  int p = mu0.n_elem; // number of latent variables
+  int k = p; // number of observed variables
+  arma::vec time_vec = arma::linspace(0, (time - 1) * delta_t, time); // time vector
 
   // Step 2: Initialize the output list
   Rcpp::List output(n);
 
   // Step 3: Generate data per individual
   for (int i = 0; i < n; i++) {
-    // Step 3.1: Create matrices of latent, observed, covariate, and id
-    // variables
+    // Step 3.1: Create matrices of latent, observed, covariate, and id variables
     arma::mat eta(p, time);
     arma::mat y(k, time);
     arma::mat x_i = x[i];
@@ -34,15 +31,11 @@ Rcpp::List SimSSMLatFixed1(const int n, const int time, const double delta_t,
     y.col(0) = eta.col(0);
     // Step 3.3: Data generation loop
     for (int t = 1; t < time; t++) {
-      eta.col(t) = alpha + (beta * eta.col(t - 1)) + (psi_l * arma::randn(p)) +
-                   (gamma_eta * x_i.col(t));
+      eta.col(t) = alpha + (beta * eta.col(t - 1)) + (psi_l * arma::randn(p)) + (gamma_eta * x_i.col(t));
       y.col(t) = eta.col(t);
     }
     // Step 3.4 Save results in a list
-    output[i] = Rcpp::List::create(
-        Rcpp::Named("id") = id, Rcpp::Named("time") = time_vec,
-        Rcpp::Named("y") = y.t(), Rcpp::Named("eta") = eta.t(),
-        Rcpp::Named("x") = x_i.t());
+    output[i] = Rcpp::List::create(Rcpp::Named("id") = id, Rcpp::Named("time") = time_vec, Rcpp::Named("y") = y.t(), Rcpp::Named("eta") = eta.t(), Rcpp::Named("x") = x_i.t());
   }
 
   // Step 4: Return results
