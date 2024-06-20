02a45a7a2499bba4372f9c86425aa1ba8be76ff7
jeksterslab <learn.jeksterslab@gmail.com>
2024-06-20 19:22:30 +0000

    Automated build.

--- tests/testthat/test-simStateSpace-sim-beta-n.R
+++ tests/testthat/test-simStateSpace-sim-beta-n.R
@@ -1,30 +1,30 @@
-## ---- test-simStateSpace-sim-phi
+## ---- test-simStateSpace-sim-beta-n
 lapply(
   X = 1,
   FUN = function(i,
                  text,
                  n) {
     message(text)
-    phi <- matrix(
+    beta <- matrix(
       data = c(
-        -0.357, 0.771, -0.450,
-        0.0, -0.511, 0.729,
-        0, 0, -0.693
+        0.7, 0.5, -0.1,
+        0.0, 0.6, 0.4,
+        0, 0, 0.5
       ),
       nrow = 3
     )
-    vcov_phi_vec_l <- t(chol(0.001 * diag(9)))
+    vcov_beta_vec_l <- t(chol(0.001 * diag(9)))
     testthat::test_that(
       text,
       {
         testthat::expect_true(
           all(
-            phi - (1 / n) * Reduce(
+            beta - (1 / n) * Reduce(
               f = `+`,
-              x = SimPhi(
+              x = SimBetaN(
                 n = n,
-                phi = phi,
-                vcov_phi_vec_l = vcov_phi_vec_l
+                beta = beta,
+                vcov_beta_vec_l = vcov_beta_vec_l
               )
             ) < 0.001
           )
@@ -32,6 +32,6 @@ lapply(
       }
     )
   },
-  text = "test-simStateSpace-sim-phi",
+  text = "test-simStateSpace-sim-beta-n",
   n = 100000
 )
