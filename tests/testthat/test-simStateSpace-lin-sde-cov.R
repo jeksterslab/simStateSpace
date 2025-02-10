359b68006e1e2ad562ec151befbec7b99b40f788
Ivan Jacob Agaloos Pesigan <jeksterslab@users.noreply.github.com>
2025-02-10 13:42:32 -0500

    Automated build.

---
+++ tests/testthat/test-simStateSpace-lin-sde-cov.R
@@ -0,0 +1,52 @@
+## ---- test-simStateSpace-lin-sde-cov
+lapply(
+  X = 1,
+  FUN = function(i,
+                 text,
+                 tol) {
+    message(text)
+    testthat::test_that(
+      text,
+      {
+        testthat::skip_on_cran()
+        phi <- matrix(
+          data = c(
+            -0.10,
+            0.05,
+            0.05,
+            -0.10
+          ),
+          nrow = 2
+        )
+        sigma <- matrix(
+          data = c(
+            2.79,
+            0.06,
+            0.06,
+            3.27
+          ),
+          nrow = 2
+        )
+        testthat::expect_true(
+          all(
+            (
+              c(
+                LinSDECov(
+                  phi = phi,
+                  sigma = sigma
+                )
+              ) - c(
+                55.554,
+                33.231,
+                33.231,
+                70.098
+              )
+            ) < tol
+          )
+        )
+      }
+    )
+  },
+  text = "test-simStateSpace-lin-sde-cov",
+  tol = 0.000001
+)
